0b7332c93c684058cea5674c9938c27b3ea5ccc7
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2015-10-27 16:49:05 -0600

    plot to the side

--- jdp/__init__.py
+++ jdp/__init__.py
@@ -1,13 +1,13 @@
 import svgwrite
 from svgwrite import cm, mm   
-
-
+import numpy as np
+from pprint import pprint
 
 class JoyDivisionPlot:
 
     def __init__(self, grid, filename='joy_division_plot.svg'):
         self.dwg  = svgwrite.Drawing(filename=filename, debug=True)
-        self.grid = grid
+        self.grid = np.array(grid)
 
     def piled_lineplots(self, stroke='gray', fill="none", stroke_width="0.5", stroke_opacity="0.2", **kwargs):
         hlines = self.dwg.add(self.dwg.g(id='hlines',
@@ -25,3 +25,25 @@ class JoyDivisionPlot:
                 x+=3
             hlines.add(self.dwg.polyline(points))
             y+=1
+
+
+    def side_plot(self, stroke='gray', fill="none", stroke_width="0.5", stroke_opacity="0.5", **kwargs):
+        sideplot = self.dwg.add(self.dwg.g(id='side',
+                                           stroke=stroke,
+                                           fill=fill,
+                                           stroke_width=stroke_width,
+                                           stroke_opacity=stroke_opacity,
+                                           **kwargs))
+
+        x = len(self.grid[0])*3
+        y = 1
+        points = []        
+        for line in self.grid:
+            print line
+            print line.max()
+            points.append( (x+line.max(), y) )
+            y+=1
+
+        pprint(points)
+        sideplot.add(self.dwg.polyline(points))
+
--- manyhist.py
+++ manyhist.py
@@ -11,9 +11,10 @@ from jdp import JoyDivisionPlot
 
 grid = []
 for l in range(100):
-    grid.append( [random.randrange(-2,2) for d in range(1,50)] )
+    grid.append( [random.randrange(0,99) for d in range(1,50)] )
 
 
 p = JoyDivisionPlot(grid)    
 p.piled_lineplots()
+p.side_plot()
 p.dwg.save()
